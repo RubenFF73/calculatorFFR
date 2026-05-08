d11751d2f019538c5655c8d5c69ccfe990263cc2
Ruben Fernandez <rubenff73@educastur.es>
2026-05-08 18:25:49 +0200

    Primer commit limpio con Jenkinsfile

---
+++ src/main/java/com/ciberFFR/calculatorFFR/CalculatorFfrApplication.java
@@ -0,0 +1,13 @@
+package com.ciberFFR.calculatorFFR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorFfrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorFfrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciberFFR/calculatorFFR/CalculatorApplicationTests.java
@@ -0,0 +1,21 @@
+package com.ciberFFR.calculatorFFR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+    private Calculator calculator = new Calculator();
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+
+}
